38c766b2ecff6cb43c3a6c9c3afb6714f661d84d
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2023-01-26 03:04:58 +0000

    Rework system

--- public/sys/desktop.js
+++ public/sys/desktop.js
@@ -1,149 +1,59 @@
-function run(self, input) { // App run function
-	function add(data) {
-		document.body.insertAdjacentHTML('beforeend', data);
-	}
-	function launchTab(source, width, height, icon, name) {
-		var x = Math.floor(Math.random() * (window.innerWidth - width))
-		var y = Math.floor(Math.random() * (window.innerHeight - height))
-		var trueHeight = height + 18;
-		add(`<section class="moveable tab" style="top: ${y}px; left: ${x}px; width: ${width}px; height: ${trueHeight}px;">
-			<header style="text-overflow: clip;">
-				<img class="verticalcenter" style="width: 16px; height: 16px" src="${icon}">
-				<span class="verticalcenter" style="color: black;">${name}</span>
-				<button class="input" onclick="closeTab(this)" style="float: right;"><span>✕</span></button>
-			</header>
-			<iframe src="${source}" style="width: ${width}px; height: ${trueHeight}px;" ></iframe>
-		</section>`)
-	}
-	
-	let fileName = self.getAttribute('name'); // Get file name
-	let fileExtension = fileName.split(".").pop(); // Get file extension
-	if (fileName.includes(".") == false) fileExtension = null;
-	let fileDir = "desktop/" + fileName; // Get file directory
-	
-	// callFile("./sys/app.json")
-	// var launch = getJsonSave("desktop/" + fileName, "exe", null);
-	// if (launch == null) return;
-	
-	if (fileExtension == null) return;
-	if (fileExtension == "lnk") { // Check if file is an app shortcut
-		launchTab(getJsonSave(fileDir, "exe", ""), getJsonSave(fileDir, "tabWidth", ""), getJsonSave(fileDir, "tabHeight", ""), getJsonSave(fileDir, "icon", ""), getJsonSave(fileDir, "tabName", ""))
-	}
-	reloadIcons()
-}
-function callFile(path) {
-	var id = "json#" + Math.floor(Math.random() * 1000000).toString();
-	add(`<iframe id="${id}" class="hidden" style="width: 0px; height: 0px; z-index: -1;" src="${path}"></iframe>`)
-	console.log(document.getElementById(id).contentDocument)
-	console.log(document.getElementById(id).contentWindow.document)
-}
-
-
-
-function closeTab(self) { // Close tab function
-	self.parentElement.parentElement.remove(); // Remove parent element
-}
-
-windowLoad()
-window.onload = window.onresize = windowLoad;
-function windowLoad() { // Load window
-	var footer = document.getElementById("footer"); // Get footer
-	footer.style.width = (window.innerWidth - 4) + "px"; // Resize footer
-	footer.style.zIndex = Math.pow(parseInt(getSave("latestZ", 1000)), 3); // Make footer stand out
-	
-	var display = document.getElementById("display");
-	display.style.width = window.innerWidth + "px"; // Resize width of display
-	display.style.height = window.innerHeight + "px"; // Resize height of display
-}
-
-
-
-document.addEventListener('mouseup', function(event) { // Mouse up event handler
-	let items = document.querySelectorAll('.moveable'); // Get all moveable objects
-	items.forEach(function(item) {
-		item.mouseoverfix = true;
-	})
-});
-
-let latestZ = parseInt(getSave("latestZ", 1000)); // Declare latestZ
-
-reloadIcons()
-function reloadIcons() { // Refresh new or add moveable properties
-	let items = document.querySelectorAll('.moveable');
-	items.forEach(function(item) {
-		item.style.zIndex = parseInt(getJsonSave("desktop/" + item.getAttribute('name'), "z", 1000));
-		
-		if (item.classList.contains("tab")) item.style.zIndex = Math.pow(latestZ, 2);
-		latestZ++;
-		setSave("latestZ", latestZ)
-		
-		item.onmousedown = function(event) { // Add item mouse down event handler
-			item.mouseoverfix = false;
-			let shiftX = event.clientX - item.getBoundingClientRect().left;
-			let shiftY = event.clientY - item.getBoundingClientRect().top;
+window.addEventListener("load", () => {
+	// Setup taskbar
+	var taskbar = document.getElementById('taskbar-items');
+	webos.taskbar = {
+		element: taskbar,
+		items: [],
+		addItem: function(icon) {
+			var img = document.createElement('img');
+			img.src = icon;
+			img.classList.add("taskbar-item");
 			
-			if (event.which !== 1) return;
-			item.style.zIndex = latestZ;
-			if (item.classList.contains("tab")) item.style.zIndex = latestZ * latestZ;
-			latestZ++;
-			document.getElementById("footer").style.zIndex = Math.pow(latestZ, 3);
-			setSave("latestZ", latestZ)
+			webos.taskbar.element.appendChild(img);
+			webos.taskbar.items.push(img);
+			return img;
+		}
+	}
 
-			moveAt(event.pageX, event.pageY);
-
-			function moveAt(pageX, pageY) { // Move item
-				item.style.left = pageX - shiftX + 'px';
-				item.style.top = pageY - shiftY + 'px';
-				if (!item.classList.contains("tab")) item.style.zIndex = Math.pow(latestZ, 4);
-			}
-			
-			function onMouseMove(event) { // Add item mouse move event handler
-				event.preventDefault();
-				// item.style.visibility = "inherit";
-				if (item.classList.contains("clickable")) {
-					item.classList.add("shadow")
-				}
-				item.style.cursor = "grabbing";
-				moveAt(event.pageX, event.pageY);
-				
-				if (!item.mouseoverfix) item.mouseoverfix = false;
-				if (item.mouseoverfix == true) {
-					item.mouseoverfix = false;
-					item.onmouseup()
-					// item.parentNode.replaceChild(item.cloneNode(true), item);
-					reloadIcons()
-				}
-			}
-
-			document.addEventListener('mousemove', onMouseMove); // Add item mouse move event handler
-
-			item.onmouseup = function() { // Add item mouse up event handler
-				item.style.cursor = null;
-				// item.style.visibility = "visible";
-				
-				// Add shadow Animation
-				if (item.classList.contains("clickable")) {
-					item.classList.remove("shadow")
-					item.classList.add("shadow-after")
-					setTimeout(function() {
-						item.classList.remove("shadow-after")
-					}, 100);
-				}
-				
-				document.removeEventListener('mousemove', onMouseMove); // Remove unneeded event handler
-				if (!item.classList.contains("tab")) item.style.zIndex = latestZ;
-				if (typeof item.getAttribute('name') !== 'undefined' && item.getAttribute('name') !== null) {
-					setJsonSave("desktop/" + item.getAttribute('name'), "x", item.style.left) // Save position
-					setJsonSave("desktop/" + item.getAttribute('name'), "y", item.style.top) // Save position
-					setJsonSave("desktop/" + item.getAttribute('name'), "z", latestZ) // Save position
-				}
-				// document.body.append(item);
-				// item.onmouseup = null;
-			};
-		};
-
-		item.ondragstart = function() {
-			return false;
-		};
+	var item = webos.taskbar.addItem(webos.assetmap.get("defaulticon"));
+	item.addEventListener("click", () => {
+		launchProgram('test_tab');
 	});
-}
+
+	setInterval(() => {
+		var time = new Date().toLocaleTimeString() + "\n" + new Date().toLocaleDateString();
+
+		document.getElementById("taskbar-time").innerText = time;
+	}, 500);
+	
+	// Setup desktop
+	var _init = function() {
+		// var { ctx } = this;
+	}
+	var _update = function({ type }) {
+		// if (type == "click") sfx('sys/assets/pop.mp3');
+	}
+	var _draw = function(fps) {
+		this.cls();
+		this.fillColor("rgb(0, 127, 255)");
+		this.fillBackground();
+	}
+
+	webos.desktopbg = new Graphics2D(document.getElementById("desktop-background"), _init, _update, _draw);
+	
+	// createContextMenu(webos.desktopbg.canvas, ({ addItem, addSpacer, remove }) => {
+	// 	addItem("eat me").addEventListener("click", () => {
+	// 		alert("i have been eaten");
+	// 		remove();
+	// 	});
+
+	// 	addSpacer();
+
+	// 	addItem("dont eat me", true);
+
+	// 	addItem("eat them").addEventListener("click", () => {
+	// 		alert("they have been eaten");
+	// 		remove();
+	// 	});
+	// })
+});
--- public/sys/system.js
+++ public/sys/system.js
@@ -1,95 +1,387 @@
-document.addEventListener('contextmenu', function(event) { // Context menu event handler
-	event.preventDefault();
-	
-	let items = document.querySelectorAll('.contextmenu'); // Get all context menus
-	items.forEach(function(item) { // Remove old context menus
-		item.remove()
-	})
-	
-	add(`<section class="contextmenu" style="left: ${event.clientX}px; top: ${event.clientY}px; z-index: ${Math.pow(parseInt(getSave("latestZ", 1000)), 5)};">
-		<ul name="contextmenu" class="menu">
-			<li name="refresh" class="menuitem"><span class="menuicon"></span>Refresh</li>
-		</ul>
-	</section>`)
-});
-document.getElementById("start").addEventListener("click", function() { // Start menu event handler
-	var numberOfButtons = 3; // Number of buttons in the start menu
-	var numberOfSeparator = 1; // Number of separator in the start menu
-	var trueHeight = (window.innerHeight - (22 + 22)) - (18 * (numberOfButtons - 1)) - (4 * (numberOfSeparator));
-	
-	add(`<section class="contextmenu" style="left: 0px; top: ${trueHeight}px; z-index: ${Math.pow(parseInt(getSave("latestZ", 1000)), 5)};">
-		<ul name="contextmenu" class="menu">
-			<li name="reinstall" class="menuitem"><span class="menuicon"></span>Reinstall</li>
-			<hr class="menuseparator">
-			<li name="refresh" class="menuitem"><span class="menuicon"></span>Reboot</li>
-			<li name="shutdown" class="menuitem"><span class="menuicon"></span>Shutdown</li>
-		</ul>
-	</section>`)
+const { Vector2, delay } = utils;
+
+let webos = window.webos = {};
+webos.highestz = 1000;
+webos.contextmenu = null;
+
+webos.assetmap = new Map();
+webos.assetmap.set("defaulticon", "/sys/assets/file.png");
+webos.assetmap.set("closebutton", "/sys/assets/close.png");
+
+webos.sfxmap = new Map();
+webos.sfxmap.set("openprogram", "/sys/assets/pop.mp3");
+
+utils.library.enable("keys");
+
+webos.uielements = [];
+utils.runAnimation(() => {
+	webos.uielements.forEach((item) => item.update());
 });
 
-document.addEventListener('mousedown', function(event) { // Button down event handler
-	let mouseOver = document.elementFromPoint(event.clientX, event.clientY);
-	function removeMenus() {
-		let items = document.querySelectorAll('.contextmenu');
-		items.forEach(function(item) {
-			item.remove()
-		})
-	}
-	if (!mouseOver.classList.contains("contextmenu") && !mouseOver.classList.contains("menu") && !mouseOver.classList.contains("menuitem") && !mouseOver.classList.contains("menuseparator") && !mouseOver.classList.contains("menuicon")) {
-		removeMenus()
-	}
-	
-	// Menu selection
-	if (mouseOver.classList.contains("menuitem") || mouseOver.classList.contains("menuicon")) {
-		if (mouseOver.classList.contains("menuicon")) mouseOver = mouseOver.parentElement;
-		var menuType = mouseOver.parentElement.getAttribute('name'); // Get menu type
-		var itemType = mouseOver.getAttribute('name'); // Get menu item type
+// System methods
+class Graphics2D {
+	constructor(canvas, init, update, draw) {
+		// Declare variables
+		this.width = canvas.width;
+		this.height = canvas.height;
+		this.canvas = canvas;
+		this.canvas.self = this;
+
+		this.ctx = canvas.getContext('2d');
+
+		this.init = init;
+		this.update = update
+		this.draw = draw;
 		
-		if (menuType == "contextmenu") { // Context menu event handler
-			if (itemType == "refresh") { // Refresh page
-				location.reload();
-			}
-			if (itemType == "reinstall") { // Reinstall system
-				window.localStorage.clear() // Clear stored data
-				location.reload()
-			}
-			if (itemType == "shutdown") { // Shutdown system
-				var newHTML = document.open("text/html", "replace"); 
-				newHTML.write("It is now safe to close your tab.");
-				newHTML.close();
-				document.body.style = `background: #000000; color: #ffff00; text-align: center; margin: 0px; padding: ${window.innerHeight / 2 + 12}px; overflow: hidden; font-family: "ＭＳ Ｐゴシック"; font-size: 20px;`;
-			}
+		// Resize the canvas
+		const resizeCanvas = () => {
+			this.canvas.width = this.width = this.canvas.parentElement.clientWidth;
+			this.canvas.height = this.height = this.canvas.parentElement.clientHeight;
 		}
-		removeMenus() // Remove any menus
-		// console.log(menuType, itemType)
+		window.addEventListener("resize", resizeCanvas);
+		resizeCanvas();
+
+		// Initialize the canvas
+		this.init();
+		utils.runAnimation(async (fps) => {
+			await this.draw(fps);
+		});
+
+		// Add event listeners
+		canvas.addEventListener("click", (e) => { this.update(e) });
+		canvas.addEventListener("dblclick", (e) => { this.update(e) });
+		canvas.addEventListener("contextmenu", (e) => { this.update(e) });
+		window.addEventListener("keydown", (e) => {
+			this.update(e)
+		});
+		window.addEventListener("keyup", (e) => {
+			this.update(e);
+		});
+		
+		// TODO:
+		// - fillCircle() Draw circle to the screen
+		// - strokeCircle() Draw ring of circle to the screen
+		// - line(x0, y0, x1, y1, col) Draw line to the screen
 	}
+	
+	cls() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+	fillColor(color) {
+		this.ctx.fillStyle = color;
+	}
+	strokeColor(color) {
+		this.ctx.strokeStyle = color;
+	}
+	setFont(font) {
+		this.ctx.font = font;
+	}
+	fillRect(x0, y0, x1, y1) {
+		this.ctx.fillRect(x0, y0, x1 - x0, y1 - y0);
+	}
+	strokeRect(x0, y0, x1, y1) {
+		this.ctx.strokeRect(x0, y0, x1 - x0, y1 - y0);
+	}
+	fillBackground() {
+		this.ctx.fillRect(0, 0, this.width, this.height);
+	}
+	async drawImage(src, x, y, w, h) {
+		var img = await loadImage(src);
+		this.ctx.drawImage(img, x, y, w || img.width, h || img.height);
+	}
+	writeText(x, y, text) {
+		this.ctx.fillText(text, x, y);
+	}
+}
+class UIElement {
+	constructor(ele) {
+		this.position = new Vector2();
+		this.zindex = 1000;
+
+		// Generate id
+		while (true) {
+			this.id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+			if (!webos.uielements.find((item) => item.id == this.id)) break;
+		}
+		
+		if (ele instanceof Element) { // Load from existing element
+			this.element = ele;
+			
+			if (/[0-9]{1,}px/g.test(ele.style.top)) {
+				this.position.y = parseFloat(ele.style.top.replace("px", ''));
+			} else {
+				this.element.style.top = "0px";
+			}
+			
+			if (/[0-9]{1,}px/g.test(ele.style.left)) {
+				this.position.x = parseFloat(ele.style.left.replace("px", ''));
+			} else {
+				this.element.style.left = "0px";
+			}
+			
+			if (!isNaN(ele.style["z-index"]) && ele.style["z-index"] !== '') {
+				this.zindex = parseFloat(ele.style["z-index"]);
+			} else {
+				ele.style["z-index"] = this.zindex;
+			}
+		} else { // Create new element
+			this.element = document.createElement('div');
+			
+			this.element.style.top = "0px";
+			this.element.style.left = "0px";
+			this.element.style["z-index"] = this.zindex;
+			
+			document.body.appendChild(this.element);
+		}
+		
+		this.element.classList.add("ui_item");
+		this.element.self = this;
+		webos.uielements.push(this);
+	}
+	
+	update() { // Update element
+		this.element.style.top = this.position.y + "px";
+		this.element.style.left = this.position.x + "px";
+		this.element.style["z-index"] = this.zindex;
+	}
+
+	remove() {
+		webos.uielements.splice(webos.uielements.indexOf(this), 1);
+		this.element.remove();
+	}
+}
+class Window extends UIElement {
+	constructor(src, options = {}) {
+		var ele = document.createElement('div');
+		ele.classList.add("window", "hidden");
+		
+		super(ele);
+		
+		this.src = src;
+		this.options = options;
+		this.dragging = false;
+		this.dragoffset = new Vector2();
+		this.loaded = false;
+		
+		this.iframe = document.createElement('iframe');
+		this.iframe.src = src;
+
+		// Create window top bar
+		var wintop = document.createElement('div');
+		wintop.classList.add("window_top");
+		if (options.color) wintop.style["background-color"] = options.color;
+		
+		var icon = document.createElement('img');
+		icon.classList.add("window_icon");
+		icon.src = this.options.icon || webos.assetmap.get("defaulticon");
+		wintop.appendChild(icon);
+
+		var title = document.createElement('span');
+		title.classList.add("window_title");
+		title.innerText = this.options.title || "Window";
+		wintop.appendChild(title);
+
+		var close = document.createElement('img');
+		close.classList.add("window_close");
+		close.src = webos.assetmap.get("closebutton");
+		close.addEventListener("click", () => {
+			this.remove();
+		});
+		wintop.appendChild(close);
+
+		var winoverlay = document.createElement('div');
+		winoverlay.classList.add("window_overlay", "hidden");
+
+		// Random position on screen
+		this.iframe.addEventListener("load", () => {
+			this.element.classList.remove("hidden");
+			this.position.x = Math.random() * (window.innerWidth - this.element.clientWidth);
+			this.position.y = Math.random() * (window.innerHeight - this.element.clientHeight - 48);
+			this.update();
+			this.focus();
+
+			this.loaded = true;
+
+			this.iframe.contentWindow.addEventListener("click", ({ target }) => {
+				this.focus();
+
+				if (webos.contextmenu == null) return;
+				if (target == webos.contextmenu) return;
+				if (isChildOf(target, webos.contextmenu)) return;
+				
+				webos.contextmenu.remove();
+				webos.contextmenu = null;
+			});
+		});
+
+		window.addEventListener("blur", () => {
+			if (document.activeElement === this.iframe) {
+				this.focus();
+			}
+		});
+		
+		if (this.options.resizable) {
+			this.element.classList.add("resizable");
+		}
+
+		// Add draggable functionality
+		wintop.addEventListener("mousedown", ({ target, button, clientX, clientY }) => {
+			if (target == title && button == 0) {
+				this.dragging = true;
+				
+				winoverlay.classList.remove("hidden");
+				this.element.classList.add("dragging");
+
+				this.focus();
+
+				this.dragoffset.x = clientX - this.position.x;
+				this.dragoffset.y = clientY - this.position.y;
+			}
+		});
+
+		window.addEventListener("mouseup", () => {
+			this.dragging = false;
+
+			winoverlay.classList.add("hidden");
+			this.element.classList.remove("dragging");
+		});
+
+		window.addEventListener("mousemove", ({ buttons, clientX, clientY }) => {
+			if (buttons !== 1) { // Cancel dragging if mouse button is not held
+				this.dragging = false;
+			}
+			if (!this.dragging) { // Cancel if not dragging
+				return;
+			}
+
+			// Update position
+			this.position.x = clientX - this.dragoffset.x;
+			this.position.y = clientY - this.dragoffset.y;
+			this.update();
+		});
+		
+		// Append elements
+		this.element.appendChild(wintop);
+		this.element.appendChild(winoverlay);
+		this.element.appendChild(this.iframe);
+		document.body.appendChild(this.element);
+	}
+
+	focus() { // Focus on window
+		this.zindex = webos.highestz;
+		webos.highestz += 1;
+	}
+}
+
+// Context menu functions
+window.addEventListener("contextmenu", (e) => {
+	e.preventDefault();
 });
 
+window.addEventListener("click", ({ target }) => {
+	if (webos.contextmenu == null) return;
+	if (target == webos.contextmenu) return;
+	if (isChildOf(target, webos.contextmenu)) return;
+	
+	webos.contextmenu.remove();
+	webos.contextmenu = null;
+});
 
-let display = document.getElementById("display");
-display.addEventListener('dragenter', handleDragOver, false);
-display.addEventListener('dragover', handleDragOver, false);
-function handleDragOver(event) {
-	event.stopPropagation();
-    event.preventDefault();
-	// console.log("whoa there")
+window.addEventListener("resize", () => {
+	if (webos.contextmenu == null) return;
 	
-	display.classList.add("highlight")
+	webos.contextmenu.remove();
+	webos.contextmenu = null;
+});
+
+function createContextMenu(target, initCallback, options = {}) {
+	target.addEventListener("contextmenu", async ({ clientX, clientY }) => {
+		if (webos.contextmenu !== null) webos.contextmenu.remove();
+
+		webos.contextmenu = document.createElement("div");
+		webos.contextmenu.classList.add("contextmenu");
+
+		await initCallback({
+			addItem: function(text, grayedout = false) {
+				var item = document.createElement("span");
+				item.classList.add("contextmenu-item");
+				item.innerHTML = text;
+				if (grayedout) item.classList.add("grayed-out");
+				webos.contextmenu.appendChild(item);
+				return item;
+			},
+			addSpacer: function(text) {
+				var line = document.createElement("div");
+				line.classList.add("contextmenu-spacer");
+				webos.contextmenu.appendChild(line);
+				return line;
+			},
+			remove: function() {
+				webos.contextmenu.remove();
+				webos.contextmenu = null;
+			}
+		});
+
+		document.body.appendChild(webos.contextmenu);
+
+		var bounds = webos.contextmenu.getBoundingClientRect();
+		webos.contextmenu.style.top = Math.min(window.innerHeight - bounds.height, options.y || clientY) + "px";
+		webos.contextmenu.style.left = Math.min(window.innerWidth - bounds.width, options.x || clientX) + "px";
+	});
 }
-display.addEventListener('dropleave', handleFileSelect, false);
-display.addEventListener('drop', handleFileSelect, false);
-function handleFileSelect(event) {
-	event.stopPropagation();
-    event.preventDefault();
+
+// System functions
+async function launchProgram(program, options = {}) {
+	var src = `/programs/${program}/index.html`;
 	
-	var fileList = event.dataTransfer.files;
-	for (let f = 0; f < fileList.length; f++) {
-		let file = fileList[f];
-		// console.log(file)
-		
-		// console.log(file.__proto__.__proto__.stream())
-		// console.log(file.__proto__.__proto__.text())
+	axios.get(`/programs/${program}/details.json`).then(({ data }) => {
+		options = Object.assign(data, options);
+
+		sfx(webos.sfxmap.get("openprogram"));
+
+		return new Window(src, options);
+	}).catch(() => {
+		console.error("Failed to retrieve program details");
+
+		sfx(webos.sfxmap.get("openprogram"));
+
+		return new Window(src, options);
+	});
+}
+
+function sfx(src) {
+	var audio;
+	if (src instanceof Audio) {
+		audio = src;
+	} else {
+		audio = new Audio(src);
 	}
+
+	// var pitch = utils.random(8, 12) / 10;
+	// audio.playbackRate = pitch;
+
+	audio.play();
+}
+
+var imgcache = {};
+function loadImage(src) {
+	if (typeof imgcache[src] !== 'undefined') return imgcache[src]; // Load image from cache
 	
-	display.classList.remove("highlight")
+	return new Promise((resolve, reject) => {
+		var img = new Image();
+		img.src = src;
+		img.onload = function() {
+			imgcache[src] = img; // Save image to cache
+			resolve(img);
+		}
+	});
+}
+
+function isChildOf(target, parent) {
+	let iterParent = target.parentElement;
+	while (iterParent) {
+		if (iterParent === parent) return true;
+		iterParent = iterParent.parentElement;
+	}
+	return false;
 }
