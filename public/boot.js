d1b66e83f5b31c4ed20963541e247fb78670c37b
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2023-01-26 19:05:22 +0000

    Update boot loader

--- public/boot.js
+++ public/boot.js
@@ -1,13 +1,52 @@
-window.webos = {};
+const webos = window.webos = {
+    loaded: false,
+    assets: new Map()
+};
 
-const bootScreen = document.getElementById("boot-screen");
+(() => {
+    const bootScreen = document.getElementById("boot-screen");
 
-// TODO: show steps taken to boot
+    const addBootText = (msg = null) => {
+        if (typeof msg == "string" && msg.length > 0) {
+            let line = document.createElement("span");
+            line.innerText = msg;
+            bootScreen.appendChild(line);
+        } else {
+            let line = document.createElement("br");
+            bootScreen.appendChild(line);
+        }
+    }
 
-window.addEventListener("load", (event) => {
-    bootScreen.classList.add("fade-out");
+    addBootText(platform.ua)
+    addBootText(new Date().toUTCString());
+    addBootText();
+    addBootText("href: " + document.location.href);
+    addBootText("origin: " + document.location.origin);
+    addBootText("protocol: " + document.location.protocol);
+    addBootText("host: " + document.location.host);
+    addBootText("hostname: " + document.location.hostname);
+    addBootText("port: " + document.location.port);
+    addBootText("pathname: " + document.location.pathname);
+    addBootText("search: " + document.location.search);
+    addBootText("hash: " + document.location.hash);
+    addBootText();
+    addBootText("Boot ... ready");
 
-    setTimeout(() => {
-        bootScreen.remove();
-    }, 500);
-});
+    window.addEventListener("DOMContentLoaded", () => {
+        addBootText("Document ... loaded");
+    });
+
+    window.addEventListener("load", () => {
+        addBootText("Resources ... loaded");
+
+        window.webos.loaded = true;
+
+        setTimeout(() => {
+            bootScreen.classList.add("fade-out");
+
+            setTimeout(() => {
+                bootScreen.remove();
+            }, 500);
+        }, 250);
+    });
+})();
