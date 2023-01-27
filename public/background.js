1a6c71225c380a0cd811db82c8b8b7f5d026afc2
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2023-01-27 22:41:06 +0000

    Added background

---
+++ public/background.js
@@ -0,0 +1,14 @@
+import { runAnimation } from "https://butterycode.com/static/js/1.2/utils.js";
+
+const canvas = document.getElementById("background");
+const ctx = canvas.getContext("2d");
+
+let hue = 0;
+
+setInterval(() => {
+    let color = `hsl(${hue}, 75%, 50%)`;
+    hue = (hue + 0.03) % 360;
+    
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}, 33.3333);
--- public/sys.js
+++ public/sys.js
@@ -1,3 +1,7 @@
 import "/calendar.js";
+import "/background.js";
+import { initElementScaler } from "https://butterycode.com/static/js/1.2/utils.js";
+
+initElementScaler();
 
 console.log(webos);
