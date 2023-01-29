40d09fdde6ef1e7a328e9df4ad3a03b997853db1
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2023-01-29 02:49:26 +0000

    Added desktop content

---
+++ public/desktop.js
@@ -0,0 +1,42 @@
+const desktopContent = document.getElementById("desktop-content");
+
+export function addDesktopIcon(src, x = 9, y = 9) {
+    let item = document.createElement("img");
+    item.src = src;
+    item.classList.add("desktop-item", "no-drag");
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    desktopContent.appendChild(item);
+    return item;
+}
+
+let dragHandler = null;
+
+window.addEventListener("mousedown", ({ target }) => {
+    if (!target.classList.contains("desktop-item")) return;
+
+    let bounds = target.getBoundingClientRect();
+    let offset = {
+        x: window.keys["MouseX"] - bounds.x,
+        y: window.keys["MouseY"] - bounds.y
+    }
+
+    dragHandler = function() {
+        if (!target.classList.contains("moving")) target.classList.add("moving");
+
+        target.style.left = `${Math.max(0, Math.min(window.innerWidth - bounds.width, window.keys["MouseX"] - offset.x))}px`;
+        target.style.top = `${Math.max(0, Math.min(window.innerHeight - bounds.height, window.keys["MouseY"] - offset.y))}px`;
+    };
+
+    window.addEventListener("mousemove", dragHandler);
+});
+
+window.addEventListener("mouseup", () => {
+    if (dragHandler != null) {
+        window.removeEventListener("mousemove", dragHandler);
+
+        for (let item of document.querySelectorAll(".desktop-item.moving")) {
+            item.classList.remove("moving");
+        }
+    }
+});
--- public/sys.js
+++ public/sys.js
@@ -1,7 +1,14 @@
 import { initElementScaler } from "https://butterycode.com/static/js/1.2/utils.js";
+import "https://butterycode.com/static/js/keys.js";
 import "/calendar.js";
 import "/background.js";
+import { addDesktopIcon } from "/desktop.js";
 
 initElementScaler();
+
+let startIcon = addDesktopIcon("/assets/start-btn.png");
+startIcon.addEventListener("dblclick", () => {
+    alert("whoa");
+});
 
 console.log(webos);
