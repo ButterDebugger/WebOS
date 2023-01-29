cb5cd45381d6f62542c4d6a9badd1b14374271e5
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2023-01-29 01:55:48 +0000

    Update menubar

--- public/sys.js
+++ public/sys.js
@@ -1,6 +1,6 @@
+import { initElementScaler } from "https://butterycode.com/static/js/1.2/utils.js";
 import "/calendar.js";
 import "/background.js";
-import { initElementScaler } from "https://butterycode.com/static/js/1.2/utils.js";
 
 initElementScaler();
 
