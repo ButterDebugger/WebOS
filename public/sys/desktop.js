d57982a4d2cdcb2db0eb249b283d0c6560fd5117
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2024-05-08 19:15:33 -0400

    Added titles to desktop items

--- public/sys/desktop.js
+++ public/sys/desktop.js
@@ -1,39 +1,65 @@
 const desktopContent = document.getElementById("desktop-content");
 
-export function addDesktopIcon(src, x = 9, y = 9) {
-    let item = document.createElement("img");
-    item.src = src;
-    item.classList.add("desktop-item", "moveable", "no-drag", "crisp");
-    item.style.left = `${x}px`;
-    item.style.top = `${y}px`;
+export class DesktopItem {
+    constructor(src, title, x = 9, y = 9) {
+        this.ele = document.createElement("div");
+        this.ele.classList.add("desktop-item", "moveable");
+        this.ele.style.left = `${x}px`;
+        this.ele.style.top = `${y}px`;
 
-    // Add drag handler
-    item.addEventListener("mousedown", () => {
-        let bounds = item.getBoundingClientRect();
-        let offset = {
-            x: window.keys["MouseX"] - bounds.x,
-            y: window.keys["MouseY"] - bounds.y
-        }
+        let icon = document.createElement("img");
+        icon.classList.add("crisp", "no-drag", "icon");
+        icon.src = src;
 
-        const dragHandler = function() {
-            if (!item.classList.contains("moving")) item.classList.add("moving");
+        let name = document.createElement("span");
+        name.classList.add("title");
+        name.innerText = title;
 
-            item.style.left = `${Math.max(0, Math.min(window.innerWidth - bounds.width, window.keys["MouseX"] - offset.x))}px`;
-            item.style.top = `${Math.max(0, Math.min(window.innerHeight - bounds.height, window.keys["MouseY"] - offset.y))}px`;
-        }
+        this.ele.appendChild(icon);
+        this.ele.appendChild(name);
 
-        document.querySelectorAll("iframe").forEach(ele => ele.classList.add("fix-drag"));
-        window.addEventListener("mousemove", dragHandler);
-        
-        window.addEventListener("mouseup", () => {
-            window.removeEventListener("mousemove", dragHandler);
-            item.classList.remove("moving");
-            document.querySelectorAll("iframe").forEach(ele => ele.classList.remove("fix-drag"));
-        }, {
-            once: true
+        // Add drag handler
+        this.ele.addEventListener("mousedown", () => {
+            let bounds = this.ele.getBoundingClientRect();
+            let offset = {
+                x: window.keys["MouseX"] - bounds.x,
+                y: window.keys["MouseY"] - bounds.y
+            }
+
+            const dragHandler = (function() {
+                if (!this.ele.classList.contains("moving")) this.ele.classList.add("moving");
+
+                this.ele.style.left = `${Math.max(0, Math.min(window.innerWidth - bounds.width, window.keys["MouseX"] - offset.x))}px`;
+                this.ele.style.top = `${Math.max(0, Math.min(window.innerHeight - bounds.height, window.keys["MouseY"] - offset.y))}px`;
+            }).bind(this);
+
+            document.querySelectorAll("iframe").forEach(ele => ele.classList.add("fix-drag"));
+            window.addEventListener("mousemove", dragHandler);
+
+            window.addEventListener("mouseup", () => {
+                console.log("what")
+                window.removeEventListener("mousemove", dragHandler);
+                this.ele.classList.remove("moving");
+                document.querySelectorAll("iframe").forEach(ele => ele.classList.remove("fix-drag"));
+            }, {
+                once: true
+            });
         });
-    });
 
-    desktopContent.appendChild(item);
-    return item;
+        desktopContent.appendChild(this.ele);
+    }
+
+    set icon(src) {
+        this.ele.querySelector(".icon").src = src;
+    }
+    get icon() {
+        return this.ele.querySelector(".icon").src;
+    }
+
+    set title(text) {
+        this.ele.querySelector(".title").innerText = text;
+    }
+    get title() {
+        return this.ele.querySelector(".title").innerText;
+    }
 }
--- public/sys/system.js
+++ public/sys/system.js
@@ -1,13 +1,13 @@
 import { timeInterval } from "./calendar.js";
 import "./background.js";
-import { addDesktopIcon } from "./desktop.js";
+import { DesktopItem } from "./desktop.js";
 import * as fs from "./fs.js";
 import Window from "./window.js";
 
 let system = {};
 
-let startIcon = addDesktopIcon("/sys/img/start-btn.png");
-startIcon.addEventListener("dblclick", () => {
+let startItem = new DesktopItem("/sys/img/start-btn.png", "Random Window");
+startItem.ele.addEventListener("dblclick", () => {
     let window = new Window("https://info.cern.ch/");
     window.title = "My Computer";
 });
@@ -23,7 +23,6 @@ Object.defineProperty(system, "zIndex", {
 });
 
 system.timeInterval = timeInterval;
-system.addDesktopIcon = addDesktopIcon;
 system.fs = fs;
 
 console.log(system);
